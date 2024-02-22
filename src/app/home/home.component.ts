7e427b90680e195e29c985fd1f5aafa5d92e0174
Balaramreddy <balaramreddy.pandillapalli@gmail.com>
2024-02-22 14:10:04 +0530

    chor:get data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,21 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent,  CommonModule],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(
-    private productsService: ProductsService
-  ) { }
-  products:Product[] =[];
+  constructor(private productsService: ProductsService) { }
+  products: Product[] = [];
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
   ngOnInit() {
     this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 20 })
-      .subscribe((products: Products) =>{
-       this.products = products.items;
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        this.products = products.items;
       })
   }
 }
