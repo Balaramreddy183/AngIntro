63a671eab86e6c878dccdaed2ae1a28ec9f8ed8b
Balaramreddy <balaramreddy.pandillapalli@gmail.com>
2024-02-23 15:54:07 +0530

    chor:added ,get,post,put,delete all completed

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,23 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Product } from '../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
-
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService } from 'primeng/api';
 
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [
-    RatingModule,FormsModule,
+    RatingModule, FormsModule, ButtonModule, ConfirmPopupModule, ToastModule
   ],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) { }
+
+  @ViewChild('deleteButton') delteButton: any;
+
   @Input() product!: Product;
-  @Output() productOutput:EventEmitter<Product> = new EventEmitter<Product>();
-  ngOnInit(){
-    this.productOutput.emit(this.product);
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+  editProduct() {
+    this.edit.emit(this.product);
+  }
+
+  confirmDelete() {
+    this.confirmationService.confirm({
+      target: this.delteButton.nativeElement,
+      message: 'Are you sure you want to delete this Product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    })
+  }
+
+  deleteProduct() {
+
+    this.delete.emit(this.product);
+  }
+
+  ngOnInit() {
+
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { options } from '../../types';
+import { Product, options } from '../../types';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,22 @@ export class ApiService {
   constructor(
     private httpClient: HttpClient,
   ) { }
-  get<T>(url:string,options:options) :Observable<T>{
-    return this.httpClient.get<T>(url,options) as Observable<T> ;
+  get<T>(url: string, options: options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
+
+
+  post<T>(url: string, body: Product, options: options): Observable<T> {
+    return this.httpClient.post<T>(url, body, options) as Observable<T>;
+  }
+
+  put<T>(url: string, body: Product, options: options): Observable<T> {
+    return this.httpClient.put<T>(url, body, options) as Observable<T>;
+  }
+
+  delete<T>(url: string, options: options): Observable<T> {
+    return this.httpClient.delete<T>(url, options) as Observable<T>;
+  }
 }
-}
+
+
